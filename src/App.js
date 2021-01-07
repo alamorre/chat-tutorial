20cc361091251b0c381d9a4b587cbac2ccc05b7e
Adam La Morre <alamorre@gmail.com>
2021-01-06 16:54:42 -0800

    no CE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
 
+import { ChatEngine } from 'react-chat-engine'
+
+import ChatFeed from './ChatFeed'
+
+const users = [
+  {
+    userName: 'Dice',
+    userSecret: 'pass1234',
+  },
+  {
+    userName: 'Domino',
+    userSecret: 'pass1234',
+  },
+  {
+    userName: 'Key',
+    userSecret: 'pass1234',
+  },
+  {
+    userName: 'Pencil',
+    userSecret: 'pass1234',
+  }
+]
+
 function App() {
+  const user = users[Math.floor(Math.random() * users.length)]
+
   return (
-    <div>
-      ok
-    </div>
+    <ChatEngine 
+      height='100vh'
+      projectID='52147d0e-0f43-4ea7-916f-1820a16bf1d7'
+      userName={user.userName}
+      userSecret={user.userSecret}
+      renderChatFeed={(props, state) => console.log(props, state)}
+    />
   );
 }
 
---
+++ src/ChatFeed/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class ChatFeed extends Component {
+    render() {
+        console.log('props', this.props)
+        return (
+            <div>feed</div>
+        );
+    }
+}
